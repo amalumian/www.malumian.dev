b5b3de9e4a4063850487e360fb90629fd50002cb
Arthur Malumian <arthurmalumian@gmail.com>
2025-07-06 18:57:19 +0700

    Migrate to Astro

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-const form = document.getElementById('search-form');
-const input = document.getElementById('search-box');
-form.addEventListener('submit', (event) => {
-  if (!input.value) {
-    event.preventDefault();
-  }
-});
