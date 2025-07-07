ee8e7e5ef2f7985c45a99d7149b746732971076a
Arthur Malumian <arthurmalumian@gmail.com>
2025-07-08 02:02:18 +0700

    Add consts from previous version of the blog

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,73 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+export const AUTHOR = 'Arthur Malumian';
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const CONTACTS = [
+	{
+		name: 'GitHub',
+		link: 'https://github.com/amalumian',
+		icon: 'github',
+	},
+	{
+		name: 'LinkedIn',
+		link: 'https://www.linkedin.com/in/amalumian',
+		icon: 'linkedin',
+	},
+	{
+		name: 'Telegram',
+		link: 'https://t.me/malumian',
+		icon: 'telegram',
+	},
+	{
+		name: 'Stack Overflow',
+		link: 'https://stackoverflow.com/users/19778080/amalumian',
+		icon: 'stack-overflow',
+	},
+	{
+		name: 'Twitter',
+		link: 'https://twitter.com/amalumian',
+		icon: 'twitter',
+	},
+	{
+		name: 'Facebook',
+		link: 'https://www.facebook.com/amalumian',
+		icon: 'facebook',
+	},
+	{
+		name: 'Reddit',
+		link: 'https://www.reddit.com/user/malumiandev',
+		icon: 'reddit',
+	},
+	{
+		name: 'YouTube',
+		link: 'https://www.youtube.com/@amalumian',
+		icon: 'youtube',
+	},
+	{
+		name: 'Mail',
+		link: 'mailto:arthurmalumian@gmail.com',
+		icon: 'email',
+	},
+	{
+		name: 'RSS',
+		link: '/feed.xml',
+		icon: 'rss',
+	},
+];
+
+export const NAVIGATION = [
+	{
+		name: 'Blog',
+		link: '/',
+	},
+	{
+		name: 'Projects',
+		link: '/projects/',
+	},
+	{
+		name: 'Setup',
+		link: '/setup/',
+	},
+	{
+		name: 'About',
+		link: '/about/',
+	},
+];
