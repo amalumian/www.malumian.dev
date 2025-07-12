cdebce937ec430cc39c4be4b9a0057ad6d1e31bf
Arthur Malumian <arthurmalumian@gmail.com>
2025-07-12 20:38:12 +0700

    Refactor code and make some refactoring

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,21 +1,21 @@
-import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import {glob} from 'astro/loaders';
+import {defineCollection, z} from 'astro:content';
 
 const blog = defineCollection({
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+  loader: glob({base: './src/content/blog', pattern: '**/*.{md,mdx}'}),
 
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			date: z.coerce.date(),
-			imageData: z.object({
-				image: image(),
-				alt: z.string(),
-				figcaption: z.string(),
-			}),
-			tags: z.array(z.string()),
-		}),
+  schema: ({image}) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      date: z.coerce.date(),
+      imageData: z.object({
+        image: image(),
+        alt: z.string(),
+        figcaption: z.string(),
+      }),
+      tags: z.array(z.string()),
+    }),
 });
 
-export const collections = { blog };
+export const collections = {blog};
