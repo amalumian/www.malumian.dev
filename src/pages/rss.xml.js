2b7802cde5f73f97cc8893505a200dcfc462355e
Arthur Malumian <arthurmalumian@gmail.com>
2025-07-12 20:36:18 +0700

    Add content to RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,27 @@
 import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, PAGES_METADATA } from '@/consts';
+import {getCollection} from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+
+import {SITE_TITLE, PAGES_METADATA} from '@/consts';
+
+const parser = new MarkdownIt();
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: PAGES_METADATA.blog.description,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+  const posts = await getCollection('blog');
+  return rss({
+    title: SITE_TITLE,
+    description: PAGES_METADATA.blog.description,
+    site: context.site,
+    trailingSlash: false,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.date,
+      link: `/blog/${post.id}/`,
+      description: post.data.description,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+      }),
+    })),
+  });
 }
