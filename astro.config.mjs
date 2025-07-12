de827243a8364cd054f6127775ce3538ff19b404
Arthur Malumian <arthurmalumian@gmail.com>
2025-07-12 20:31:23 +0700

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,29 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import { remarkReadingTime } from './plugins/remark-reading-time.mjs';
+import {remarkReadingTime} from './plugins/remark-reading-time.mjs';
 
 export default defineConfig({
-	site: 'https://www.malumian.dev',
-	integrations: [
-		mdx(),
-		sitemap({
-			filter: (page) => !page.includes('tags'),
-		}),
-	],
-	markdown: {
-		shikiConfig: {
-			theme: 'github-dark-default',
-		},
-		remarkPlugins: [remarkReadingTime],
-	},
-	devToolbar: {
-		enabled: false,
-	},
+  site: 'https://www.malumian.dev',
+  redirects: {
+    '/about': '/',
+  },
+	prefetch: true,
+  integrations: [
+    mdx(),
+    sitemap({
+      filter: (page) => !page.includes('tags'),
+    }),
+  ],
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark-default',
+    },
+    remarkPlugins: [remarkReadingTime],
+  },
+  devToolbar: {
+    enabled: false,
+  },
 });
