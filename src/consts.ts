d4827ffb627e11060b8ea06a9c499cb3c9a4d4cc
Arthur Malumian <arthurmalumian@gmail.com>
2025-07-12 20:35:53 +0700

    Update consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,93 +1,100 @@
 export const SITE_TITLE = 'Arthur Malumian';
 
 export const PAGES_METADATA = {
-	home: {
-		title: 'Home',
-		description:
-			'Arthur Malumian is a programmer who loves web technologies. He works as a Front End Developer and writes posts on his blog.',
-	},
-	blog: {
-		title: 'Blog',
-		description:
-			"This is the blog of Arthur Malumian, a programmer who posts about software development and more. He shares about the books he's reading, the movies he's watching, and the trips he's taking.",
-	},
-	projects: {
-		title: 'Projects',
-		description: "This page provides an overview of Arthur Malumian's projects.",
-	},
-	setup: {
-		title: 'Setup',
-		description: "This page provides an overview of Arthur Malumian's personal setup.",
-	},
-	404: {
-		title: '404',
-		description: 'Page not found.',
-	},
+  home: {
+    title: 'Home',
+    description:
+      'Arthur Malumian is a programmer who loves web technologies. He works as a Front End Developer and writes posts on his blog.',
+  },
+  blog: {
+    title: 'Blog',
+    description:
+      "This is the blog of Arthur Malumian, a programmer who posts about software development and more. He shares about the books he's reading, the movies he's watching, and the trips he's taking.",
+  },
+  tags: {
+    title: 'Tags',
+    description:
+      "This is the blog of Arthur Malumian, a programmer who posts about software development and more. He shares about the books he's reading, the movies he's watching, and the trips he's taking.",
+  },
+  projects: {
+    title: 'Projects',
+    description:
+      "This page provides an overview of Arthur Malumian's projects.",
+  },
+  setup: {
+    title: 'Setup',
+    description:
+      "This page provides an overview of Arthur Malumian's personal setup.",
+  },
+  404: {
+    title: '404',
+    description: 'Page not found.',
+  },
 };
 
 export const ROUTES = [
-	{
-		name: 'Home',
-		link: '/',
-	},
-	{
-		name: 'Blog',
-		link: '/blog',
-	},
-	{
-		name: 'Projects',
-		link: '/projects',
-	},
-	{
-		name: 'Setup',
-		link: '/setup',
-	},
+  {
+    name: 'Home',
+    link: '/',
+  },
+  {
+    name: 'Blog',
+    link: '/blog',
+  },
+  {
+    name: 'Projects',
+    link: '/projects',
+  },
+  {
+    name: 'Setup',
+    link: '/setup',
+  },
 ];
 
 export const SOCIALS = [
-	{
-		name: 'GitHub',
-		link: 'https://github.com/amalumian',
-		icon: 'github',
-	},
-	{
-		name: 'LinkedIn',
-		link: 'https://www.linkedin.com/in/amalumian',
-		icon: 'linkedin',
-	},
-	{
-		name: 'Telegram',
-		link: 'https://t.me/malumian',
-		icon: 'telegram',
-	},
-	{
-		name: 'Stack Overflow',
-		link: 'https://stackoverflow.com/users/19778080/amalumian',
-		icon: 'stack-overflow',
-	},
-	{
-		name: 'Twitter',
-		link: 'https://twitter.com/amalumian',
-		icon: 'twitter',
-	},
-	{
-		name: 'Facebook',
-		link: 'https://www.facebook.com/amalumian',
-		icon: 'facebook',
-	},
-	{
-		name: 'Reddit',
-		link: 'https://www.reddit.com/user/malumiandev',
-		icon: 'reddit',
-	},
-	{
-		name: 'YouTube',
-		link: 'https://www.youtube.com/@amalumian',
-		icon: 'youtube',
-	},
-	{
-		name: 'RSS',
-		link: '/rss.xml',
-		icon: 'rss',
-	},
+  {
+    name: 'GitHub',
+    link: 'https://github.com/amalumian',
+    icon: 'github',
+  },
+  {
+    name: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/amalumian',
+    icon: 'linkedin',
+  },
+  {
+    name: 'Telegram',
+    link: 'https://t.me/malumian',
+    icon: 'telegram',
+  },
+  {
+    name: 'Stack Overflow',
+    link: 'https://stackoverflow.com/users/19778080/amalumian',
+    icon: 'stack-overflow',
+  },
+  {
+    name: 'Twitter',
+    link: 'https://twitter.com/amalumian',
+    icon: 'twitter',
+  },
+  {
+    name: 'Facebook',
+    link: 'https://www.facebook.com/amalumian',
+    icon: 'facebook',
+  },
+  {
+    name: 'Reddit',
+    link: 'https://www.reddit.com/user/malumiandev',
+    icon: 'reddit',
+  },
+  {
+    name: 'YouTube',
+    link: 'https://www.youtube.com/@amalumian',
+    icon: 'youtube',
+  },
+  {
+    name: 'RSS',
+    link: '/rss.xml',
+    icon: 'rss',
+  },
 ];
